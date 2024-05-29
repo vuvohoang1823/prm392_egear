fc15c6f10efb68eb7fbc593c1c17221a7e062aab
Tran Minh Dat <tranminhdat0805@gmail.com>
2024-05-29 21:20:39 +0700

    create 3 tab

--- app/src/main/java/com/example/egear/MainActivity.java
+++ app/src/main/java/com/example/egear/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.egear;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +10,45 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonFragHome, buttonFragProfile, buttonFragSettings;
 
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      EdgeToEdge.enable(this);
-      setContentView(R.layout.activity_main);
-      ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-         Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-         v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-         return insets;
-      });
-   }
+    private Button currentButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        buttonFragHome = (Button) findViewById(R.id.btnFragHome);
+        buttonFragProfile = (Button) findViewById(R.id.btnFragProfile);
+        buttonFragSettings = (Button) findViewById(R.id.btnFragSetting);
+
+        buttonFragHome.setOnClickListener(view -> {
+            getSupportFragmentManager().beginTransaction().replace(R.id.mainLayout, new HomeFragment()).commit();
+            if (currentButton != null) {
+                currentButton.setBackgroundColor(getResources().getColor(R.color.white)); // replace 'defaultButtonColor' with your default button color
+            }
+            view.setBackgroundColor(getResources().getColor(R.color.cyan));
+            currentButton = (Button) view;
+        });
+
+        buttonFragProfile.setOnClickListener(view -> {
+            getSupportFragmentManager().beginTransaction().replace(R.id.mainLayout, new ProfileFragment()).commit();
+            if (currentButton != null) {
+                currentButton.setBackgroundColor(getResources().getColor(R.color.white)); // replace 'defaultButtonColor' with your default button color
+            }
+            view.setBackgroundColor(getResources().getColor(R.color.cyan));
+            currentButton = (Button) view;
+        });
+
+        buttonFragSettings.setOnClickListener(view -> {
+            getSupportFragmentManager().beginTransaction().replace(R.id.mainLayout, new SettingFragment()).commit();
+            if (currentButton != null) {
+                currentButton.setBackgroundColor(getResources().getColor(R.color.white)); // replace 'defaultButtonColor' with your default button color
+            }
+            view.setBackgroundColor(getResources().getColor(R.color.cyan));
+            currentButton = (Button) view;
+        });
+    }
 }

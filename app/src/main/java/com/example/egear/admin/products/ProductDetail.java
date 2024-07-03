77c03019f0a905e444ac075db3badfa67d3ed3a0
vuvohoang1823 <vuvohoang1823@gmail.com>
2024-07-03 11:46:54 +0700

    Edit + Add

--- app/src/main/java/com/example/egear/admin/products/ProductDetail.java
+++ app/src/main/java/com/example/egear/admin/products/ProductDetail.java
@@ -42,9 +42,6 @@ public class ProductDetail extends AppCompatActivity {
          @Override
          public void onClick(View v) {
             // Thực hiện chức năng sửa sản phẩm
-                  Intent intent = new Intent(ProductDetail.this, ProductEditActivity.class);
-                  startActivity(intent);
-
          }
       });
 

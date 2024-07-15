aa5255c8a7a98b2b3bac5c0d9bb05f5a30201c60
dat <tranminhdat0805@gmail.com>
2024-07-15 23:00:49 +0700

    Fix version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,10 +36,10 @@ android {
 }
 
 dependencies {
-    implementation ("com.google.android.gms:play-services-maps:18.2.0")
-    implementation ("com.google.android.gms:play-services-location:21.3.0")
-    implementation ("com.google.maps.android:android-maps-utils:2.2.5")
-    implementation ("com.android.volley:volley:1.2.1")
+    implementation(libs.playServicesMaps)
+    implementation(libs.playServicesLocation)
+    implementation(libs.androidMapsUtils)
+    implementation(libs.volley)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
